11798dfd627d59f876cbcdee945b17ced674402d
Huaiping He <1424957713@qq.com>
2020-11-24 20:59:38 +0800

    完善代码

--- src/main/java/com/nnt/excel/databse/CountDataList.java
+++ src/main/java/com/nnt/excel/databse/CountDataList.java
@@ -4,7 +4,9 @@ import com.nnt.excel.model.CountData;
 import lombok.Data;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author soulmate
@@ -14,6 +16,53 @@ public class CountDataList {
     /**
      * 输出台帐统计数据
      */
-    public static final List<CountData> countDataList = new ArrayList<>(2);
+    private static final List<CountData> countDataList = new ArrayList<>(2);
 
+    private static final Map<String, CountData> countDataMap = new HashMap<>(8);
+
+    public static void addCountData(CountData countData, String checkTime, int item) {
+        // 如果包含当前检测人数据
+        if(countDataMap.containsKey(countData.getId())) {
+            setCheckTime(countDataMap.get(countData.getId()), checkTime, item);
+        }else {
+            // 没有当前检测人数据
+            setCheckTime(countData, checkTime, item);
+        }
+        // 放入到Map中
+        countDataMap.put(countData.getId(), countData);
+    }
+
+    private static void setCheckTime(CountData countData, String checkTime, int item) {
+        switch (item){
+            case 0 : {
+                countData.setCheckOne(checkTime);
+                break;
+            }
+            case 1 : {
+                countData.setCheckTwo(checkTime);
+                break;
+            }
+            case 2 : {
+                countData.setCheckThree(checkTime);
+                break;
+            }
+            case 3 : {
+                countData.setCheckFour(checkTime);
+                break;
+            }
+            default: {
+                break;
+            }
+        }
+    }
+
+    /**
+     * 获取统计列表
+     */
+    public static List<CountData> getCountData() {
+        countDataMap.forEach((id, dataObject) -> {
+            countDataList.add(dataObject);
+        });
+        return countDataList;
+    }
 }
--- src/main/java/com/nnt/excel/web/ExcelFileController.java
+++ src/main/java/com/nnt/excel/web/ExcelFileController.java
@@ -1,15 +1,19 @@
 package com.nnt.excel.web;
 
+import com.fasterxml.jackson.databind.util.JSONPObject;
+import com.nnt.excel.databse.CheckDataList;
+import com.nnt.excel.databse.CountDataList;
+import com.nnt.excel.databse.PersonData;
+import com.nnt.excel.model.CheckData;
+import com.nnt.excel.model.CountData;
+import org.springframework.boot.jackson.JsonObjectSerializer;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.ModelAndView;
 
-import java.lang.management.MonitorInfo;
+import java.util.List;
+
 
 /**
  * @author soulmate
@@ -17,15 +21,37 @@ import java.lang.management.MonitorInfo;
 @Controller
 public class ExcelFileController {
 
-    @PostMapping("/exportExcel")
+    @RequestMapping("/exportExcel")
     @ResponseBody
-    public void exportExcel() {
+    public Object exportExcel() {
         // 遍历人员数据
+        PersonData.personList.forEach(person -> {
+            // 遍历检测数据
+            for (int i = 0; i < CheckDataList.checkDataList.size(); i++) {
 
-        // 遍历检测数据
+                for (CheckData checkData : CheckDataList.checkDataList.get(i)) {
+                    // 身份数据相同
+                    if(person.getId().equals(checkData.getId())) {
+                        CountData countData = new CountData();
+                        countData.setId(person.getId());
+                        countData.setName(person.getName());
+                        countData.setAddr(person.getAddr());
+                        countData.setPhone(checkData.getPhone());
+                        // 添加到数据统计列表中
+                        CountDataList.addCountData(countData, checkData.getCheckTime(), i);
+                        return;
+                    }
+                }
+            }
 
+        });
         // 生成台帐
+        List<CountData> countData = CountDataList.getCountData();
+        System.out.println(countData);
+        return countData;
     }
+
+
 
     @RequestMapping("/")
     public String index() {
